36d780c2502764fa48cd54dbc17faedc94b79c78
Guillermo Garcia-Costoya <ggarciacosto@gmail.com>
2024-02-12 11:06:57 -0800

    re-run simulations and plot figure 2

--- re_run_sims_&_plot_fig2.R
+++ re_run_sims_&_plot_fig2.R
@@ -1,4 +1,6 @@
 
+## Re-runnning simulations for VS et al. response ##
+
 ## Packages ----
 
 library(mvtnorm)
@@ -10,157 +12,6 @@ library(data.table)
 library(DescTools)
 
 registerDoParallel(detectCores()) # Register parallel cores
-
-## Reviewer code ----
-
-# sims 
-K <- 1000
-
-# Simulating data from a multivariate analysis
-G <- diag(5)
-G[1, 5] <- G[5, 1] <- G[2, 5] <- G[5, 2] <- 0.75
-G[5, 4] <- G[4, 5] <- -0.75
-
-x_good <- rmvnorm(K, rep(0, 5), G)
-
-#Simulation data `a la GC et al.
-e_gc <- rmvnorm(K, rep(0, 5), diag(5))
-x_gc <- apply(e_gc, 1, \(vec) { vec %*% G }) |> t()
-
-
-matrixStats::colVars(x_good)
-matrixStats::colVars(x_gc)
-
-## Testing my original vs reviewers code ----
-
-# randomization of base tpt values
-randomize_tpts <- function(tpts, corr, s){
-  
-  # build basic genetic correlations matrix
-  gmtx <- diag(length(tpts))
-  
-  # modify genetic correlations matrix depending on tradeoff
-  
-  # gsto
-  if(corr %in% c("gsto", "both")){gmtx[5,1] <- gmtx[1,5] <- gmtx[5,2] <- gmtx[2,5] <- s}
-  if(corr %in% c("gsto", "both")){gmtx[5,4] <- gmtx[4,5] <- -s}
-  
-  # tde
-  if(corr %in% c("tde", "both")){gmtx[5,3] <- gmtx[3,5] <- s}
-  
-  # initial amount of change in tpts
-  initial_change <- c(rnorm(4), rnorm(1, sd = 1))
-  
-  # amount of change due to genetic correlations
-  change <- initial_change %*% gmtx
-  
-  # new tpt values
-  new_tpts <- as.vector(tpts + change)
-  
-  # correct for impossible values
-  
-  # mid
-  new_tpts[2] <- ifelse(new_tpts[2] > new_tpts[3] - 1, new_tpts[3] - 1, new_tpts[2])
-  
-  # ctmin
-  new_tpts[1] <- ifelse(new_tpts[1] > new_tpts[2] - 1, new_tpts[1] - 1, new_tpts[1])
-  
-  # ctmax
-  new_tpts[4] <- ifelse(new_tpts[4] < new_tpts[3] + 1, new_tpts[3] + 1, new_tpts[4])
-  
-  # round final values to two decimal places
-  new_tpts <- round(new_tpts, digits = 2)
-  
-  return(new_tpts)
-  
-}
-
-# modified
-randomize_tpts <- function(tpts, corr, s){
-  
-  # build basic genetic correlations matrix
-  gmtx <- diag(length(tpts))
-  
-  # Simulating data from a multivariate analysis
-  if(corr %in% c("gsto", "both")){gmtx[5,1] <- gmtx[1,5] <- gmtx[5,2] <- gmtx[2,5] <- s}
-  if(corr %in% c("gsto", "both")){gmtx[5,4] <- gmtx[4,5] <- -s}
-  
-  # tde
-  if(corr %in% c("tde", "both")){gmtx[5,3] <- gmtx[3,5] <- s}
-  
-  # define change
-  change <- suppressWarnings(rmvnorm(1, rep(0, 5), gmtx))
-  
-  # new tpt values
-  new_tpts <- as.vector(tpts + change)
-  
-  # correct for impossible values
-  
-  # mid
-  new_tpts[2] <- ifelse(new_tpts[2] > new_tpts[3] - 1, new_tpts[3] - 1, new_tpts[2])
-  
-  # ctmin
-  new_tpts[1] <- ifelse(new_tpts[1] > new_tpts[2] - 1, new_tpts[1] - 1, new_tpts[1])
-  
-  # ctmax
-  new_tpts[4] <- ifelse(new_tpts[4] < new_tpts[3] + 1, new_tpts[3] + 1, new_tpts[4])
-  
-  # round final values to two decimal places
-  new_tpts <- round(new_tpts, digits = 2)
-  
-  return(new_tpts)
-  
-  
-}
-
-ctmin <- 10
-ctmax <- 32.5
-topt <- 25
-midpoint <- mean(c(topt, ctmin))
-pmax <- 10
-tpts <- c(ctmin, midpoint, topt, ctmax, pmax)
-scorr <- 0.5 # strength of genetic correlations
-
-
-gsto <- randomize_tpts(tpts, corr = "gsto", s = scorr)
-for(i in 1:1000){ gsto <- rbind(gsto, randomize_tpts(tpts, corr = "gsto", s = scorr))}
-matrixStats::colVars(gsto)
-
-tde <- randomize_tpts(tpts, corr = "tde", s = scorr)
-for(i in 1:1000){ tde <- rbind(tde, randomize_tpts(tpts, corr = "tde", s = scorr))}
-matrixStats::colVars(tde)
-
-none <- randomize_tpts(tpts, corr = "none", s = scorr)
-for(i in 1:1000){ none <- rbind(none, randomize_tpts(tpts, corr = "none", s = scorr))}
-matrixStats::colVars(none)
-
-both <- randomize_tpts(tpts, corr = "both", s = scorr)
-for(i in 1:1000){ both <- rbind(both, randomize_tpts(tpts, corr = "both", s = scorr))}
-matrixStats::colVars(both)
-
-gsto <- as.data.frame(gsto)
-colnames(gsto) <- c("ctmin", "mid", "topt", "ctmax", "pmax")
-gsto <- gsto %>% mutate(corr = "gsto")
-
-tde <- as.data.frame(tde)
-colnames(tde) <- c("ctmin", "mid", "topt", "ctmax", "pmax")
-tde <- tde %>% mutate(corr = "tde")
-
-none <- as.data.frame(none)
-colnames(none) <- c("ctmin", "mid", "topt", "ctmax", "pmax")
-none <- none %>% mutate(corr = "none")
-
-both <- as.data.frame(both)
-colnames(both) <- c("ctmin", "mid", "topt", "ctmax", "pmax")
-both <- both %>% mutate(corr = "both") 
-
-original <- rbind(gsto, tde, none, both)
-
-original %>% 
-  ggplot(aes(y = pmax, x = topt, col = corr)) +
-  #geom_point() +
-  geom_smooth(method = "lm")
-
 
 ## Functions to generate populations ----
 
@@ -180,7 +31,7 @@ randomize_tpts <- function(tpts, corr, s){
   # define change in traits due to genetic correlations
   # note that the change is now calculated via 
   # note that suppressWarnings is added to suppress messages related to the
-  # definitiveness of the matrix
+  # definitiveness of the matrix when S = 0.75, but thisissue is not ignored. 
   change <- suppressWarnings(rmvnorm(1, rep(0, 5), gmtx))
   
   # new tpt values
@@ -255,7 +106,7 @@ generate_population <- function(reps, n, tpts, corr, s){
     # loop for each replicate
     pop <- foreach(j = 1:n,
                    combine = rbind,
-                   .packages = "tidyverse",
+                   .packages = c("tidyverse","mvtnorm"),
                    .export = c("randomize_tpts", "build_tpd", "generate_tpc")) %dopar% {
                      
                      tpc <- nest(generate_tpc(build_tpd(randomize_tpts(tpts = tpts, corr = corr, s = s))))
@@ -286,17 +137,21 @@ midpoint <- mean(c(topt, ctmin)) # Fourth polygon point
 
 tpts <- c(ctmin, midpoint, topt, ctmax, pmax)
 
-scorr <- 0.75 # strength of genetic correlations
-
 ## Generate new populations ----
 
-# N = 500
-none_500 <- generate_population(reps, tpts = tpts, n = 50, corr = "none", s = 0)
-gsto_500 <- generate_population(reps, tpts = tpts, n = 50, corr = "gsto", s = scorr)
-tde_500 <- generate_population(reps, tpts = tpts, n = 50, corr = "tde", s = scorr)
-both_500 <- generate_population(reps, tpts = tpts, n = 50, corr = "both", s = scorr)
+# With S = 0.75
+none_0.75 <- generate_population(reps, tpts = tpts, n = 50, corr = "none", s = 0.75)
+gsto_0.75 <- generate_population(reps, tpts = tpts, n = 50, corr = "gsto", s = 0.75)
+tde_0.75 <- generate_population(reps, tpts = tpts, n = 50, corr = "tde", s = 0.75)
+both_0.75 <- generate_population(reps, tpts = tpts, n = 50, corr = "both", s = 0.75)
 
-## Functions to generate temperature sequences ----
+# With S = 0.5
+none_0.5 <- generate_population(reps, tpts = tpts, n = 50, corr = "none", s = 0.5)
+gsto_0.5 <- generate_population(reps, tpts = tpts, n = 50, corr = "gsto", s = 0.5)
+tde_0.5 <- generate_population(reps, tpts = tpts, n = 50, corr = "tde", s = 0.5)
+both_0.5 <- generate_population(reps, tpts = tpts, n = 50, corr = "both", s = 0.5)
+
+## Function to generate temperature sequences ----
 
 generate_tseq <- function(reps, gen, days, mean_0, sd_0, mean_c, sd_c, burnin){
   
@@ -483,43 +338,74 @@ parallel_simulation <- function(pops, tseqs, corr_name, tseq_name){
   
 }
 
-## Run and simulations ----
+## Run simulations ----
 
-# run and bind simulations
-both_sim <- parallel_simulation(pops = both_pops, tseqs = rcp8.5, corr_name = "GSTO + TDE", tseq_name = "RCP 8.5")
-none_sim <- parallel_simulation(pops = none_pops, tseqs = rcp8.5, corr_name = "None", tseq_name = "RCP 8.5")
-tde_sim <- parallel_simulation(pops = tde_pops, tseqs = rcp8.5, corr_name = "TDE", tseq_name = "RCP 8.5")
-gsto_sim <- parallel_simulation(pops = gsto_pops, tseqs = rcp8.5, corr_name = "GSTO", tseq_name = "RCP 8.5")
-sims <- rbind(none_sim, gsto_sim, tde_sim, both_sim)
+# run and bind simulations with S = 0.75
+both_0.75_sim <- parallel_simulation(pops = both_0.75, tseqs = rcp8.5, corr_name = "GSTO + TDE", tseq_name = "RCP 8.5")
+none_0.75_sim <- parallel_simulation(pops = none_0.75, tseqs = rcp8.5, corr_name = "None", tseq_name = "RCP 8.5")
+tde_0.75_sim <- parallel_simulation(pops = tde_0.75, tseqs = rcp8.5, corr_name = "TDE", tseq_name = "RCP 8.5")
+gsto_0.75_sim <- parallel_simulation(pops = gsto_0.75, tseqs = rcp8.5, corr_name = "GSTO", tseq_name = "RCP 8.5")
+sims_0.75 <- rbind(none_0.75_sim, gsto_0.75_sim, tde_0.75_sim, both_0.75_sim)
+
+# run and bind simulations with S = 0.5
+both_0.5_sim <- parallel_simulation(pops = both_0.5, tseqs = rcp8.5, corr_name = "GSTO + TDE", tseq_name = "RCP 8.5")
+none_0.5_sim <- parallel_simulation(pops = none_0.5, tseqs = rcp8.5, corr_name = "None", tseq_name = "RCP 8.5")
+tde_0.5_sim <- parallel_simulation(pops = tde_0.5, tseqs = rcp8.5, corr_name = "TDE", tseq_name = "RCP 8.5")
+gsto_0.5_sim <- parallel_simulation(pops = gsto_0.5, tseqs = rcp8.5, corr_name = "GSTO", tseq_name = "RCP 8.5")
+sims_0.5 <- rbind(none_0.5_sim, gsto_0.5_sim, tde_0.5_sim, both_0.5_sim)
 
 ## Plotting ----
 
-## Average population size plot ----
+# loadding and processing original simulation results
+load("data/sum_sim_results.RData")
+sims_o_0.75 <- sum_sim_results %>% filter(tseq == "high", n0_k == 50) %>%
+  rename(avg_pop_size = n) %>% 
+  mutate(for_what = "OP", s = 0.75) %>% 
+  dplyr::select(for_what, s, corr, gen, avg_pop_size)
+sims_o_0.75$corr <- as.factor(sims_o_0.75$corr)
+levels(sims_o_0.75$corr) <- list(`GSTO + TDE` = "both", GSTO = "gsto", None = "none", TDE = "tde")
+
+# adding columns for joint plotting 
+sims_0.5 <- sims_0.5 %>% mutate(for_what = "TN", s = 0.5)
+sims_0.75 <- sims_0.75 %>% mutate(for_what = "TN", s = 0.75)
+
+# combining simulation results
+sims <- rbind(sims_0.5, sims_0.75)
 
 # generate data with all generations
 gen <- seq(0,85,1)
 pop_n <- seq(1,10,1)
 tseq_n <- seq(1,10,1)
 corrs <- c("None", "GSTO", "TDE", "GSTO + TDE")
-all_gens <- expand.grid(corrs, gen, pop_n, tseq_n)
-colnames(all_gens) <- c("corr", "gen", "pop_n", "tseq_n")
+for_what <- "TN"
+s <- c(0.5, 0.75)
+all_gens <- expand.grid(for_what, s, corrs, gen, pop_n, tseq_n)
+colnames(all_gens) <- c("for_what", "s", "corr", "gen", "pop_n", "tseq_n")
 
 # get population sizes
-n_plot <- sims %>% group_by(corr, gen, pop_n, tseq_n) %>% summarise(pop_size = n())
+n_plot <- sims %>% group_by(for_what, s, corr, gen, pop_n, tseq_n) %>% 
+  summarise(pop_size = n())
 
 # merge with all generations data & set N = 0 in generations where no data was recorded
-n_plot <- merge(n_plot, all_gens, by = c("corr", "gen", "pop_n", "tseq_n"), all = TRUE)
+n_plot <- merge(n_plot, all_gens, by = c("for_what", "s", "corr", "gen", "pop_n", "tseq_n"), all = TRUE)
 n_plot$pop_size <- ifelse(is.na(n_plot$pop_size), 0, n_plot$pop_size)
 
 # find average population size by correlation and generation
-n_plot <- n_plot %>% group_by(corr, gen) %>% summarise(avg_pop_size = mean(pop_size)) 
+n_plot <- n_plot %>% group_by(for_what, s, corr, gen) %>% summarise(avg_pop_size = mean(pop_size)) 
+
+# bind with original simulation results
+n_plot <- rbind(n_plot, sims_o_0.75)
 
 # set correlations as a factor and define colors
-n_plot$corr <- factor(tests_plot$corr, levels = c("None", "GSTO","TDE","GSTO + TDE"))
+n_plot$corr <- factor(n_plot$corr, levels = c("None", "GSTO","TDE","GSTO + TDE"))
 colors <- c("None" = "#009E73", "GSTO" = "#56B4E9","TDE" = "#CC79A7", "GSTO + TDE" = "#D55E00")
 
 # plot
 n_plot %>% 
+  mutate(where = ifelse(for_what == "TN", "Re-run simulation", "Original simulation")) %>%
+  mutate(s_name = paste("Correlation strength (S) =", s)) %>%
+  #filter(s == 0.75) %>% # Filter for only original vs re-run comparison
+  filter(for_what == "TN") %>% # Filter for only re-run comparison between S
   ggplot(aes(x = gen, y = avg_pop_size)) +
   geom_rect(xmin = 0, xmax = 5, ymin = -Inf, ymax = Inf, alpha = 0.5, fill = "lightgray") +
   geom_hline(yintercept = 0, linetype = 2, linewidth = 1.25) +
@@ -528,26 +414,30 @@ n_plot %>%
   scale_color_manual(values = colors) +
   xlab("Generation") +
   ylab("Mean population size (N)") +
+  #facet_grid(cols = vars(where)) + # add or remove for original vs re-run comparisons
+  facet_grid(cols = vars(s_name)) + # add or remove for S comparisons
+  #facet_wrap(~where*s_name) + # add or remove to show all results
   theme_minimal() +
   theme(axis.line = element_line(),
         axis.ticks = element_line(),
-        legend.position = c(0.25, 0.25),
+        legend.position = c(0.15, 0.25),
         legend.title = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(linewidth = 0.5)) 
+        panel.grid.major = element_line(linewidth = 0.5), 
+        strip.text = element_text(size = 12)) +
+  #ggtitle("Correlation strenght (S) = 0.75") # title for original vs re-run comparisons
+  ggtitle("Using reviewer's methodology") # title for S comparisons
 
 
 
 
-## Initial trait distribution ----
 
-sims %>% 
-  filter(gen == 0) %>% 
-  dplyr::select(corr, topt, pmax, ctmin, ctmax) %>%
-  pivot_longer(!corr, names_to = "trait", values_to = "value") %>%
-  ggplot(aes(x = value)) +
-  geom_density(aes(fill = corr, col = corr), alpha = 0.25) +
-  facet_wrap(~trait, scales = "free_x")
 
-sims_50 <- sims
-save(sims_50, file = 'data/sims_50.RData')
+
+
+
+
+
+
+
+
